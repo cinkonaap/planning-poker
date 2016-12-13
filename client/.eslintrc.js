8e66f222e31de3f745e43bfb32a1d114c6be028f
Marcin Horoszko <marcin.horoszko@airhelp.com>
2016-12-13 12:44:38 +0100

    linter clinet

---
+++ client/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: 'eslint-config-airhelp/ember-config',
+  globals: {
+    d3: false,
+    Dropzone: false,
+    goog_event: false,
+    gtm_event: false,
+    mixpanel: false,
+    module: false,
+    nv: false,
+    require: false,
+  },
+};
