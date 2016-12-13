0c573b7aa4c641e22d2cdb7dfcc7680c4c997a9d
Marcin Horoszko <marcin.horoszko@airhelp.com>
2016-12-13 18:15:20 +0100

    redirecting if not logged

--- client/app/services/state.js
+++ client/app/services/state.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   users: [],
+  currentUser: null,
+
   _hasUserWithName(name) {
     return this.get('users').any(user => user.name === name);
   },
+
   createUser(name) {
     if (this._hasUserWithName(name)) {
       return false;
@@ -14,6 +17,7 @@ export default Ember.Service.extend({
       return user;
     }
   },
+
   setCurrentUser(user) {
     // TODO maybe we will need a set here
     this.set('currentUser', user);
